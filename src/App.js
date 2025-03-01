41609fe5f263294c70bd270e42fdc44f5a77eb3f
Aefvvges <torri.barbara4@gmail.com>
2025-03-01 19:36:12 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -10,9 +10,22 @@ function App() {
 
   const [input, setInput] = useState('');
 
-  const agregarInput = val => {
-    setInput(input + val);
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
+
+  const manejarInput = (valor) => {
+    const ultimoCaracter = input.slice(-1);
+    if (input === '' && esOperador(valor)){
+      return;
+    }
+
+    if (esOperador(ultimoCaracter) && esOperador(valor)) {
+      setInput(input.slice(0,-1) + valor);
+    } else {
+      setInput(input + valor);
+    }
+  }
 
   const calcularResultado = useCallback(() => {
     if (input) {
@@ -42,28 +55,28 @@ function App() {
       <div className='contenedor-calculadora'>
         <Pantalla input={input}/>
         <div className='fila'>
-          <Boton manejarClic={agregarInput}>1</Boton>
-          <Boton manejarClic={agregarInput}>2</Boton>
-          <Boton manejarClic={agregarInput}>3</Boton>
-          <Boton manejarClic={agregarInput}>+</Boton>
+          <Boton manejarClic={manejarInput}>1</Boton>
+          <Boton manejarClic={manejarInput}>2</Boton>
+          <Boton manejarClic={manejarInput}>3</Boton>
+          <Boton manejarClic={manejarInput}>+</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClic={agregarInput}>4</Boton>
-          <Boton manejarClic={agregarInput}>5</Boton>
-          <Boton manejarClic={agregarInput}>6</Boton>
-          <Boton manejarClic={agregarInput}>-</Boton>          
+          <Boton manejarClic={manejarInput}>4</Boton>
+          <Boton manejarClic={manejarInput}>5</Boton>
+          <Boton manejarClic={manejarInput}>6</Boton>
+          <Boton manejarClic={manejarInput}>-</Boton>          
         </div>
         <div className='fila'>
-          <Boton manejarClic={agregarInput}>7</Boton>
-          <Boton manejarClic={agregarInput}>8</Boton>
-          <Boton manejarClic={agregarInput}>9</Boton>
-          <Boton manejarClic={agregarInput}>*</Boton>
+          <Boton manejarClic={manejarInput}>7</Boton>
+          <Boton manejarClic={manejarInput}>8</Boton>
+          <Boton manejarClic={manejarInput}>9</Boton>
+          <Boton manejarClic={manejarInput}>*</Boton>
         </div>
         <div className='fila'>
           <Boton manejarClic={calcularResultado}>=</Boton>
-          <Boton manejarClic={agregarInput}>0</Boton>
-          <Boton manejarClic={agregarInput}>.</Boton>
-          <Boton manejarClic={agregarInput}>/</Boton>
+          <Boton manejarClic={manejarInput}>0</Boton>
+          <Boton manejarClic={manejarInput}>.</Boton>
+          <Boton manejarClic={manejarInput}>/</Boton>
         </div>
         <div className='fila'>
           <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -3,12 +3,8 @@ import '../hojas-de-estilo/Boton.css';
 
 function Boton(props) {
 
-  const esOperador = valor => {
-    return isNaN(valor) && (valor !== '.') && (valor !== '=');
-  };
-
   return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+    <div className={`boton-contenedor`}
     onClick={() => props.manejarClic(props.children)}>
       {props.children}
     </div>
